011c742100e2ca644901d982b017601f618f125f
Kaweees <49963287+Kaweees@users.noreply.github.com>
2020-06-16 12:04:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ board = [
     [0,7,0,3,0,0,0,1,2],
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
-    ] 
+    ] # 2-d array that represents the sudoku board
 
 display(board)
 
-if solve(board):
+if solve(board): # if the board is solvable 
     print("solved board:")
     display(board)
-else:
+else: # if the board is unsolvable 
     print("task failed sucessfully")
