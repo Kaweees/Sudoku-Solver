9a480013ac41654e58119d8afa227f721c1fa5aa
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2021-10-04 00:14:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from display import display as display 
 from solve import *
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-    ] # 2-d array that represents the sudoku board
+board = [[7,8,0,4,0,0,1,2,0],
+  [6,0,0,0,7,5,0,0,9],
+  [0,0,0,6,0,1,0,7,8],
+  [0,0,7,0,4,0,2,6,0],
+  [0,0,1,0,5,0,9,3,0],
+  [9,0,4,0,6,0,0,0,5],
+  [0,7,0,3,0,0,0,1,2],
+  [1,2,0,0,0,7,4,0,0],
+  [0,4,9,2,0,6,0,0,7]
+  ] # 2-d array that represents the sudoku board
 display(board)
 
 if solve(board): # if the board is solvable 
-    print("solved board:")
-    display(board)
+  print("solved board:")
+  display(board)
 else: # if the board is unsolvable 
-    print("task failed sucessfully")
+  print("task failed sucessfully")
